4645f0b803e6c50fcdbd79cdc9937412959c797d
Harald Rietman <h.rietman@portofrotterdam.com>
2017-06-30 19:14:32 +0200

    Working config node and xiaomi-ht node, added package.json

--- node-red-contrib-xiaomi-gateway/xiaomi-gateway.js
+++ node-red-contrib-xiaomi-gateway/xiaomi-gateway.js
@@ -3,12 +3,9 @@ module.exports = function(RED) {
     function RemoteServerNode(n) {
         RED.nodes.createNode(this, n);
         this.name = n.name;
-        this.group = n.group;
-        this.port = n.port;
-        this.udpv = n.udpv;
+        this.deviceList = n.deviceList || [];
 
         var node = this;
-
     }
 
     RED.nodes.registerType("xiaomi-gateway", RemoteServerNode);
--- node-red-contrib-xiaomi-ht/xiaomi-ht.js
+++ node-red-contrib-xiaomi-ht/xiaomi-ht.js
@@ -17,44 +17,12 @@ module.exports = function(RED) {
         node.status({fill:"grey",shape:"ring",text:"battery"});
 
         if (this.gateway) {
-
-            var opts = {type: node.gateway.udpv, reuseAddr:true};
-            var server;
-
-            if (!udpInputPortsInUse.hasOwnProperty(node.gateway.port)) {
-                node.log("New UDP socket");
-                server = dgram.createSocket(opts);  // default to udp4
-                udpInputPortsInUse[node.gateway.port] = server;
-            }
-            else {
-                node.warn(RED._("udp.errors.alreadyused", node.gateway.port));
-                server = udpInputPortsInUse[node.gateway.port];  // re-use existing
-            }
-
-            server.on('listening', function () {
-                server.setBroadcast(true);
-                try {
-                    server.setMulticastTTL(128);
-                    server.addMembership(node.gateway.group, null);
-                    node.log(RED._("udp.status.mc-group",{group:node.gateway.group}));
-
-                } catch (e) {
-                    if (e.errno == "EINVAL") {
-                        node.error(RED._("udp.errors.bad-mcaddress"));
-                    } else if (e.errno == "ENODEV") {
-                        node.error(RED._("udp.errors.interface"));
-                    } else {
-                        node.error(RED._("udp.errors.error",{error:e.errno}));
-                    }
-                }
-            });
-
-            server.on('message', function(msg, rinfo) {
-                var payload = JSON.parse(msg);
-
+            node.on('input', function(msg) {
+                // var payload = JSON.parse(msg);
+                var payload = msg.payload;
                 node.log("Received message from: " + payload.model + " sid: " + payload.sid + " payload: " + payload.data);
 
-                if (payload.sid == node.sid) {
+                if (payload.sid == node.sid && payload.model == "sensor_ht") {
                     var data = JSON.parse(payload.data)
 
                     if (data.voltage) {
@@ -65,8 +33,6 @@ module.exports = function(RED) {
                         } else {
                             node.status({fill:"green",shape:"dot",text:"battery"});
                         }
-                    } else {
-                        node.status({fill:"grey",shape:"ring",text:"battery"});
                     }
 
                     if (node.output == "0") {
@@ -100,29 +66,14 @@ module.exports = function(RED) {
                 }
             });
 
-            node.on("input", function(msg) {
-                // console.log("got input");
-            });
-
             node.on("close", function() {
-                if (udpInputPortsInUse.hasOwnProperty(node.gateway.port)) {
-                    delete udpInputPortsInUse[node.gateway.port];
-                }
-                server.close();
             });
-
-            try { server.bind(node.gateway.port, null); }
-            catch(e) { } // Don't worry if already bound
 
         } else {
             // no gateway configured
         }
 
     }
-
-    RED.httpAdmin.get('/udp-ports/:id', RED.auth.needsPermission('udp-ports.read'), function(req,res) {
-        res.json(Object.keys(udpInputPortsInUse));
-    });
 
     RED.nodes.registerType("xiaomi-ht", XiaomiHtNode);
 
